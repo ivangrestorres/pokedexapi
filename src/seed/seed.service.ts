8239574c59cc75c66caf0aa2dac39d3e5fe79967
Ivan Gres Torres <ivangrestorres@gmail.com>
2023-10-13 00:25:57 +0200

    insertmany

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,23 +1,34 @@
 import { Injectable } from '@nestjs/common';
 import { PokeResponse } from './interfaces/poke-response.interface';
+import { InjectModel } from '@nestjs/mongoose';
+import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class SeedService {
-  constructor() {}
+  constructor(
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>,
+  ) {}
 
   async exectuteSeed() {
     const fetchResponse = await fetch(
-      'https://pokeapi.co/api/v2/pokemon?limit=15',
+      'https://pokeapi.co/api/v2/pokemon?limit=650',
     );
     const { results } = (await fetchResponse.json()) as PokeResponse;
+
+    const pokemonsToInsert: { name: string; no: number }[] = [];
+
+    await this.pokemonModel.deleteMany({});
 
     results.map(({ name, url }) => {
       const segments = url.split('/');
       const no: number = +segments[segments.length - 2];
 
-      console.log({ name, no });
+      pokemonsToInsert.push({ no, name });
     });
 
+    await this.pokemonModel.insertMany(pokemonsToInsert);
     return { message: 'Seed executed' };
   }
 }
